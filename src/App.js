f35167c2c3f3a0c5780aa02b7c42da0424c9a1ae
devanshu757 <devanshumaru@gmail.com>
2025-07-04 15:32:32 +0530

    Changes in card designs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import React from "react";
-import { BrowserRouter, Routes } from "react-router-dom";
+import { HashRouter, Routes } from "react-router-dom";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 // import { useAuth } from "./Components/AuthContext";
@@ -13,17 +13,17 @@ function App() {
   // const { isLoggedIn } = useAuth();
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className="App">
         <Header />
         <Routes>
-  {CommonRoutes}
-  {ProtectedRoutes}
-</Routes>
+          {CommonRoutes}
+          {ProtectedRoutes}
+        </Routes>
 
         <Footer />
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
